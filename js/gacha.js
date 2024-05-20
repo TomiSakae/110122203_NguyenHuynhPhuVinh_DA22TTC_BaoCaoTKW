542a08b8bfbb4277427ac743379cabb5e13de362
Hungtvu113 <tomisakaeanime@gmail.com>
2024-05-20 17:29:24 +0700

    chỉnh lỗi roll

--- js/gacha.js
+++ js/gacha.js
@@ -1,6 +1,6 @@
-let gacha = localStorage.getItem("gacha");
+let gacha = parseInt(localStorage.getItem("gacha"));
 
-if (gacha == null) {
+if (isNaN(gacha)) {
     gacha = 0;
     localStorage.setItem("gacha", 0);
 }
@@ -15,24 +15,9 @@ let vi_tri_url = urlParts[dem - 2];
 
 switch (vi_tri_url) {
     case "wuthering_waves":
-        gacha++;
-        localStorage.setItem("gacha", gacha);
-        break;
     case "azur_promilia":
-        gacha++;
-        localStorage.setItem("gacha", gacha);
-        break;
-
     case "arknights_endfield":
-        gacha++;
-        localStorage.setItem("gacha", gacha);
-        break;
-
     case "project_mugen":
-        gacha++;
-        localStorage.setItem("gacha", gacha);
-        break;
-
     case "zenless_zone_zero":
         gacha++;
         localStorage.setItem("gacha", gacha);
@@ -42,7 +27,14 @@ switch (vi_tri_url) {
 document.getElementById("so_gacha").textContent = gacha;
 
 function KTGacha(so_lan) {
+    let gacha = parseInt(localStorage.getItem("gacha"));  // Lấy lại giá trị từ localStorage
+    document.getElementById("noi_gacha").innerHTML = `<p>Bạn không đủ vé quay!</p>`;
     if (gacha >= so_lan) {
+        let lan_gacha = localStorage.getItem("lan_gacha");
+        if (isNaN(lan_gacha)) {
+            lan_gacha = 0;
+            localStorage.setItem("lan_gacha", 0);
+        }
         gacha -= so_lan;
         localStorage.setItem("gacha", gacha);
         document.getElementById("so_gacha").textContent = gacha;
@@ -58,27 +50,28 @@ function KTGacha(so_lan) {
         let ten_sp;
         for (let i = 0; i < so_lan; i++) {
             randomInteger = Math.floor(Math.random() * (max - min + 1)) + min;
-
+            lan_gacha++;
+            if (lan_gacha == 10) {
+                randomInteger = Math.floor(Math.random() * (30 - 1 + 1)) + min;
+                lan_gacha = 0;
+            }
+            localStorage.setItem("lan_gacha", lan_gacha);
             if (randomInteger <= 6) {
                 url_anh = "assets/anime-rezero-puck-and-emilia-pack.png";
                 ten_sp = "Ảnh nền chuột Emilia x1";
-            }
-            else if (randomInteger <= 30 && randomInteger > 6) {
+            } else if (randomInteger <= 30 && randomInteger > 6) {
                 if (randomInteger <= 18 && randomInteger > 6) {
                     url_anh = "assets/konosuba-megumin-and-staff-pack.png";
                     ten_sp = "Ảnh nền chuột Megumin x1";
-                }
-                else if (randomInteger > 18 && randomInteger <= 30) {
+                } else if (randomInteger > 18 && randomInteger <= 30) {
                     url_anh = "assets/miss-kobayashi-s-dragon-maid-kanna-and-dragon-form-pack.png";
                     ten_sp = "Ảnh nền chuột Kanna x1";
                 }
-            }
-            else if (randomInteger > 30) {
+            } else if (randomInteger > 30) {
                 if (randomInteger > 30 && randomInteger <= 50) {
                     url_anh = "assets/gold_key.png";
                     ten_sp = "Chìa khóa vàng x1";
-                }
-                else if (randomInteger > 50) {
+                } else if (randomInteger > 50) {
                     url_anh = "assets/sliver_key.png";
                     ten_sp = "Chìa khóa bạc x1";
                 }
